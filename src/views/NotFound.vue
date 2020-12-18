c804e64723b71ac1efa2f642d1101ab8f957624e
elisedzambranoM <45537925+elisedzambranoM@users.noreply.github.com>
2020-12-18 19:00:58 -0300

    se instalo vuetify y se creo la vista Not Found

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="d-flex justify-center">
+        <v-img src="../assets/NotFoundImage.jpg" max-width="700"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
